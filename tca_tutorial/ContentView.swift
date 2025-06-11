cff75f6605e847fe9abd70b5b95bc29e7ebab4bf
Jean <gofltmxj@naver.com>
2025-06-11 17:08:01 +0900

    Initial Commit

---
+++ tca_tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tca_tutorial
+//
+//  Created by Heawon Seo on 6/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tca_tutorial/tca_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_tutorialApp.swift
+//  tca_tutorial
+//
+//  Created by Heawon Seo on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct tca_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
